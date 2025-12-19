637b97f42e79f0d54e4d871d5aa62521b4a5b1b9
Dododoc <kuukuu2657@gmail.com>
2025-12-19 22:22:19 +0900

    2

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -69,6 +69,7 @@ public class PlayerStats : MonoBehaviour
 
         UpdateUI(); 
 
+
         if (UIManager.instance != null && GameManager.instance != null)
         {
             UIManager.instance.UpdateEvolutionUI((int)GameManager.instance.currentCharacter);
